5f94025f29dc635ed6f19bda9a23d417395ff273
didaci <didaci@unica.it>
2026-04-09 14:20:07 +0200

    make_delete

---
+++ src/timeslot_2/cmake-build-debug/CMakeFiles/timeslot_2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/timeslot_2.dir/main.c.o"
+  "CMakeFiles/timeslot_2.dir/main.c.o.d"
+  "CMakeFiles/timeslot_2.dir/timeslot.c.o"
+  "CMakeFiles/timeslot_2.dir/timeslot.c.o.d"
+  "timeslot_2"
+  "timeslot_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/timeslot_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
